e4a5be61fa5e2278b373b8eae0becaa95aca4b90
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-07 22:07:58 +0530

    backend ready for deployment

--- Backend/database/knexFile.js
+++ Backend/database/knexFile.js
@@ -1,12 +1,21 @@
+// export default {
+//   development: {
+//     client: "pg",
+//     connection: {
+//       host: "localhost",
+//       user: "postgres",
+//       password: "postgres",
+//       database: "postgres",
+//       port: 5432
+//     }
+//   }
+// };
+
+
+
 export default {
   development: {
     client: "pg",
-    connection: {
-      host: "localhost",
-      user: "postgres",
-      password: "postgres",
-      database: "postgres",
-      port: 5432
-    }
+    connection: process.env.DATABASE_URL
   }
 };
