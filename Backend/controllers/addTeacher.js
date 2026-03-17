a445b6b3fb9b00792a9217d8e4bea51ca7bb69fb
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-17 11:15:22 +0530

    change puppeteer-core

--- Backend/controllers/addTeacher.js
+++ Backend/controllers/addTeacher.js
@@ -5,8 +5,9 @@ export default async function addTeacherData(req, res)  {
         const teacherinfo = req.body.teacherinfo;
         const { id, ...data } = teacherinfo;
         await db("profile_master").insert(data);
+
         res.status(201).json({
-            message: "Data created successfully"
+            message: " Created successfully"
         });
     }
     catch (err) {
