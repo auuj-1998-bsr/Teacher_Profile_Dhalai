755219c516d8809d9e0a8c60240cfac237bd35c2
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-19 12:30:55 +0530

    uploads routes change backend

--- Backend/routes/teacherRoutes.js
+++ Backend/routes/teacherRoutes.js
@@ -4,7 +4,7 @@ import addTeacherData from "../controllers/addTeacher.js"
 import updateTeacher from "../controllers/updateTeacher.js"
 import { checkAllFields } from "../Middleware/fieldMiddleware.js";
 import { loginTeacher } from "../controllers/login.js";
-import { upload } from "../Middleware/uploadImage.js";
+import { uploads } from "../Middleware/uploadImage.js";
 import { getdashboard } from "../controllers/dashboard.js";
 
 const router = express.Router();
@@ -12,6 +12,6 @@ router.post("/login", loginTeacher);
 router.post("/dashboard", getdashboard);
 router.post("/profile", getTeacherProfile);
 router.post("/addTeacherData", checkAllFields, addTeacherData);
-router.post("/updateTeacherinfo", upload.single("image"), updateTeacher);
+router.post("/updateTeacherinfo", uploads.single("image"), updateTeacher);
 
 export default router;
