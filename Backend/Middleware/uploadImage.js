cd38fd4c4ba4a0e33e8c9c3c52c362e25a6e67b4
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-19 12:05:38 +0530

    rename  uploads file

--- Backend/Middleware/uploadImage.js
+++ Backend/Middleware/uploadImage.js
@@ -20,4 +20,4 @@ const uploadsize = multer({
 
 export const uploads = multer({uploadsize, storage });
 
-///multer is a node-js middleware for handling multipart/form-data, uploading(file,image pdf)....///////  
+///multer is a node-js middleware for handling multipart/form-data, uploading(file,image pdf)....  
