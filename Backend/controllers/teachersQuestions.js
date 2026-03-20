e18b223fba307f1aa0b5ea0827a4cf59876fef98
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-20 14:12:12 +0530

    change quiz

--- Backend/controllers/teachersQuestions.js
+++ Backend/controllers/teachersQuestions.js
@@ -1,11 +1,12 @@
     import db from "../database/db.js";
      export async function teacherQuestions(req, res) {
-        try {
-            const allQuestion = await db("questions").select("*");
-            res.status(200).json({ allQuestion })
-        }
-        catch (err) {
-            res.status(400).json({ error: err.message });
-        }
-
-    }
+       try {
+    const allQuestion = await db("questions").select("*");
+    res.status(200).json({ allQuestion });
+  } catch (err) {
+    res.status(500).json({
+      error: "Internal server error",
+      details: err.message
+    });
+  }
+}
