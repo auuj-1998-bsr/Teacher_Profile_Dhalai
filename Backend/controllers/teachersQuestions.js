15d542c238720b589c8868ea819ced61035c47a8
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-25 16:38:35 +0530

    complete status update change api

--- Backend/controllers/teachersQuestions.js
+++ Backend/controllers/teachersQuestions.js
@@ -26,7 +26,8 @@ export async function teacherQuestions(req, res) {
                 .where("teacher_code", teacher_code)
                 .first();
                 const result="Completed";
-             if (resultData.result_status === "Pass") {
+                console.log("resultData:", resultData);
+             if (resultData && resultData.result_status === "Pass") {
             return res.status(200).json({ result });
         }
             else {
