b69f67a22d1f8491ba39f9709fe9f0e4a756dd84
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-27 16:50:03 +0530

    Backend knexx updatte insert result

--- Backend/controllers/teachersQuestions.js
+++ Backend/controllers/teachersQuestions.js
@@ -4,49 +4,60 @@ export async function teacherQuestions(req, res) {
     const { resultStatus, score, teacher_code, attempt } = req.body;
     if (resultStatus && score !== undefined) {
         try {
-            await db("result").insert({
-                result_status: resultStatus,
-                score: score,
-                teacher_code: teacher_code,
-                attempt: attempt
-            });
-            res.status(201).json({ message: "Result Updated" })
-        }
+            if (teacher_code) {
+                await db("result")
+                    .where("teacher_code", teacher_code)
+                    .update({
+                        result_status: resultStatus,
+                        score: score,
+                        attempt: attempt
+                    });
+                     res.status(201).json({ message: "Result Updated" })
+                }
+              else{  await db("result").insert({
+                    result_status: resultStatus,
+                    score: score,
+                    teacher_code: teacher_code,
+                    attempt: attempt
+                });
+                res.status(201).json({ message: "Result Updated" })
+            }
+            }
         catch (err) {
-            console.error("ERROR:", err);
-            res.status(500).json({
-                error: "Result not Updated server Error",
-                details: err.message
-            });
-        }
-    }
-    else {
-        try {
-            const resultData = await db("result")
-                .where("teacher_code", teacher_code)
-                .first();
-             if (resultData && resultData.result_status === "Pass") {
-            return res.status(200).json({ resultData });
-        }
-            else {
-                const allData = await db("questions as q")
-                    .join("options as o", "q.id", "o.question_id")
-                    .select(
-                        "q.id as question_id",
-                        "q.question",
-                        "o.id",
-                        "o.option_text",
-                        "o.is_correct"
-                    );
-                return res.status(200).json({ allData }); 
+                console.error("ERROR:", err);
+                res.status(500).json({
+                    error: "Result not Updated server Error",
+                    details: err.message
+                });
             }
         }
-        catch (err) {
-            console.error("ERROR:", err);
-            res.status(500).json({
-                error: "Internal server error",
-                details: err.message
-            });
+    else {
+            try {
+                const resultData = await db("result")
+                    .where("teacher_code", teacher_code)
+                    .first();
+                if (resultData && resultData.result_status === "Pass") {
+                    return res.status(200).json({ resultData });
+                }
+                else {
+                    const allData = await db("questions as q")
+                        .join("options as o", "q.id", "o.question_id")
+                        .select(
+                            "q.id as question_id",
+                            "q.question",
+                            "o.id",
+                            "o.option_text",
+                            "o.is_correct"
+                        );
+                    return res.status(200).json({ allData });
+                }
+            }
+            catch (err) {
+                console.error("ERROR:", err);
+                res.status(500).json({
+                    error: "Internal server error",
+                    details: err.message
+                });
+            }
         }
-    }
-} 
+    } 
