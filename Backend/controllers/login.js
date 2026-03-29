14a19bc354ba9cdda428c32cbda4659ef6991a7a
Anuj Kumar <nikanujrajput1947@gmail.com>
2026-03-29 16:22:42 +0530

    login type update

--- Backend/controllers/login.js
+++ Backend/controllers/login.js
@@ -6,37 +6,42 @@ import { UAParser } from "ua-parser-js";
 export const loginTeacher = async (req, res) => {
   const { loginType, teacher_code, password } = req.body;
   const hash = await bcrypt.hash("123456", 10);
-  const admin=loginType;
+  const admin = loginType;
   const teacher = await db("profile_master").where("teacher_code", teacher_code).first();
-try{
-  if (!teacher||admin==="admin")
-    return res.status(401).json({ message: "Adim or Teacher not found" });
+  try {
+    if (loginType === "teacher" && !teacher) {
+      return setError("Teacher not found");
+    }
 
-  const isMatch = await bcrypt.compare(password, hash);
-  if (!isMatch)
-    return res.status(401).json({ message: "Wrong password" });
+    if (loginType === "admin" && admin !== "admin") {
+      return setError("Invalid admin");
+    }
 
-  const token = jwt.sign({teacher_code:teacher_code, teacher_name: teacher. },
-    "mysecret123",
-    { expiresIn: "1d" }
-  );
+    const isMatch = await bcrypt.compare(password, hash);
+    if (!isMatch)
+      return res.status(401).json({ message: "Wrong password" });
 
-  //login info 
-  const parser = new UAParser(req.headers['user-agent']);
-  const result = parser.getResult();
-  const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket?.remoteAddress;
-  const browser = `${result.browser.name} ${result.browser.version}`;
-  const device = `${result.os.name} ${result.os.version}`;
-  console.log(result);
-  await db("login_logs").insert({
-    teacher_code: teacher_code,
-    ip_address: ip,
-    browser: browser,
-    device: device,
-  })
-  res.json({ token, teacher: { name: teacher.teacher_name }, message: "Login_Success" });
-}
-catch(err){
-  res.status(500).json({message:err});
-}
+    const token = jwt.sign({ teacher_code: teacher_code, teacher_name: teacher. },
+      "mysecret123",
+      { expiresIn: "1d" }
+    );
+
+    //login info 
+    const parser = new UAParser(req.headers['user-agent']);
+    const result = parser.getResult();
+    const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket?.remoteAddress;
+    const browser = `${result.browser.name} ${result.browser.version}`;
+    const device = `${result.os.name} ${result.os.version}`;
+    console.log(result);
+    await db("login_logs").insert({
+      teacher_code: teacher_code,
+      ip_address: ip,
+      browser: browser,
+      device: device,
+    })
+    res.json({ token, teacher: { name: teacher.teacher_name }, message: "Login_Success" });
+  }
+  catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
